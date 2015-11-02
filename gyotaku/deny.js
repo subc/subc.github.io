7733654631cafe63853a738d1b836ad3909103bc
ikeda <ikeda@000196-M3.local>
2015-11-02 14:32:15 +0900

    Web魚拓拒否スクリプト

--- gyotaku/deny.js
+++ gyotaku/deny.js
@@ -5,6 +5,8 @@ function main(){
     // ローカルなら動かさない
     if ($(location).attr('hostname') == ''){
         console.info('is local');
+
+        disable();
         return null;
     }
 
@@ -14,8 +16,25 @@ function main(){
         return null;
     }else{
         console.info('copy site');
+        disable();
     }
 }
+
+function disable(){
+    console.info('start disable');
+    // div class mainを非表示に
+    // div class
+    // div.mainを書き換え
+    $("div.main").html("<h3>本ページは削除されました。</h3>");
+    // redirect
+    setTimeout(function(){
+        $(document).ready( function() {
+            url = "http://megalodon.jp/";
+            $(location).attr("href", url);
+        });
+    },15000);
+}
+
 
 function foo2(){ alert ('2'); }
 
